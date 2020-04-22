b424ae8cc45c80f01d1d1fb75c203efbf7469df1
pinkipail <fyodorov_vasya@list.ru>
2020-04-22 18:23:17 +0300

    Updates

---
+++ precache-manifest.57afff5753c3d602cef3745baf0c23d5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca080c5fe0023ea8e30eb805cb05f5b",
+    "url": "/JavaScript/index.html"
+  },
+  {
+    "revision": "beb8f2fe530fc1109096",
+    "url": "/JavaScript/static/css/main.7447ba1b.chunk.css"
+  },
+  {
+    "revision": "d31fbc6b89f27b6d4088",
+    "url": "/JavaScript/static/js/2.5273ec53.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/JavaScript/static/js/2.5273ec53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb8f2fe530fc1109096",
+    "url": "/JavaScript/static/js/main.fb64cdce.chunk.js"
+  },
+  {
+    "revision": "faa9e238419a0e13afd1",
+    "url": "/JavaScript/static/js/runtime-main.6a486c04.js"
+  },
+  {
+    "revision": "a707a6f2897d07ba48d6d3afd645126f",
+    "url": "/JavaScript/static/media/btn-add-active.a707a6f2.svg"
+  },
+  {
+    "revision": "1540e1f000484edb9e3fd9f6bf8267c1",
+    "url": "/JavaScript/static/media/btn-add.1540e1f0.svg"
+  },
+  {
+    "revision": "3756f6908696786567ff3f919f1e92bb",
+    "url": "/JavaScript/static/media/btn-edit-active.3756f690.svg"
+  },
+  {
+    "revision": "480bd40c46c53a10812b5ceb468866a1",
+    "url": "/JavaScript/static/media/btn-edit.480bd40c.svg"
+  },
+  {
+    "revision": "2074f588598d11d56aea695ff3c57085",
+    "url": "/JavaScript/static/media/btn-remove-active.2074f588.svg"
+  },
+  {
+    "revision": "4acd55d1586fb9a426d3b6918ede9e57",
+    "url": "/JavaScript/static/media/btn-remove.4acd55d1.svg"
+  },
+  {
+    "revision": "8b75c836a89694637ba213236daf4bcb",
+    "url": "/JavaScript/static/media/btn-select-active.8b75c836.svg"
+  },
+  {
+    "revision": "4d64993b64968f0a12dddf16da25d813",
+    "url": "/JavaScript/static/media/btn-select.4d64993b.svg"
+  }
+]);
