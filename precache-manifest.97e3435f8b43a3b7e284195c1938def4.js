abb3c5ae3aac3ec319cb11902bf6c58658a164d3
pinkipail <fyodorov_vasya@list.ru>
2020-05-13 17:32:41 +0300

    Updates

---
+++ precache-manifest.97e3435f8b43a3b7e284195c1938def4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8508737580e664958bece0693426c3",
+    "url": "/digital-pass/index.html"
+  },
+  {
+    "revision": "431476801d383a8560e1",
+    "url": "/digital-pass/static/css/main.8c0175e0.chunk.css"
+  },
+  {
+    "revision": "d31fbc6b89f27b6d4088",
+    "url": "/digital-pass/static/js/2.5273ec53.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/digital-pass/static/js/2.5273ec53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "431476801d383a8560e1",
+    "url": "/digital-pass/static/js/main.8e226bc1.chunk.js"
+  },
+  {
+    "revision": "cbf553f21cfd428a5ac2",
+    "url": "/digital-pass/static/js/runtime-main.8805f188.js"
+  },
+  {
+    "revision": "a707a6f2897d07ba48d6d3afd645126f",
+    "url": "/digital-pass/static/media/btn-add-active.a707a6f2.svg"
+  },
+  {
+    "revision": "1540e1f000484edb9e3fd9f6bf8267c1",
+    "url": "/digital-pass/static/media/btn-add.1540e1f0.svg"
+  },
+  {
+    "revision": "3756f6908696786567ff3f919f1e92bb",
+    "url": "/digital-pass/static/media/btn-edit-active.3756f690.svg"
+  },
+  {
+    "revision": "480bd40c46c53a10812b5ceb468866a1",
+    "url": "/digital-pass/static/media/btn-edit.480bd40c.svg"
+  },
+  {
+    "revision": "2074f588598d11d56aea695ff3c57085",
+    "url": "/digital-pass/static/media/btn-remove-active.2074f588.svg"
+  },
+  {
+    "revision": "4acd55d1586fb9a426d3b6918ede9e57",
+    "url": "/digital-pass/static/media/btn-remove.4acd55d1.svg"
+  },
+  {
+    "revision": "8b75c836a89694637ba213236daf4bcb",
+    "url": "/digital-pass/static/media/btn-select-active.8b75c836.svg"
+  },
+  {
+    "revision": "4d64993b64968f0a12dddf16da25d813",
+    "url": "/digital-pass/static/media/btn-select.4d64993b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/JavaScript/precache-manifest.da6a4e7628f0b40290f535048e435d93.js"
+  "/digital-pass/precache-manifest.97e3435f8b43a3b7e284195c1938def4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JavaScript/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/digital-pass/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
